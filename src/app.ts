cc7b28ebfb58214c731a11885ed9e0ecf80a471d
Damien Hardy (digimangos) <49808393+digimangos@users.noreply.github.com>
2024-10-02 18:45:54 +0100

    Implement swagger UI

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import { setRoutes } from "./routes/itemRoutes";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
+
+const swaggerDocument = require("./swagger.json");
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Register item routes
 setRoutes(app);
